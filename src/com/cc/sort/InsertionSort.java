ef43b4ab89d20ff206afb534838102d7ae7cda2e
HJKCC <chencheng0816@gmail.com>
2018-04-17 22:26:39 +0800

    create the simple insertionSort

--- src/com/cc/sort/InsertionSort.java
+++ src/com/cc/sort/InsertionSort.java
@@ -1,7 +1,15 @@
 package com.cc.sort;
 
 public class InsertionSort {
-	public static <E extends Comparable<? super E>> void insertionSort(E[] e) {
-		
+	public static <E extends Comparable<? super E>> void sort(E[] e) {
+		int j;
+		int length = e.length;
+		for (int i = 1; i < length; i++) {
+			E tmp = e[i]; 
+			for (j = i; j > 0 && e[j].compareTo(e[j - 1]) < 0; j--) {
+				e[j] = e[j - 1];
+			}
+			e[j] = tmp;
+		}
 	}
 }
