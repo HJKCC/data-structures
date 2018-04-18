382f870d7a30603a18c8d5c9ca32761156e3dc9b
HJKCC <chencheng0816@gmail.com>
2018-04-18 14:17:34 +0800

    create the shellSort

--- src/com/cc/sort/InsertionSort.java
+++ src/com/cc/sort/InsertionSort.java
@@ -1,15 +1,33 @@
 package com.cc.sort;
 
 public class InsertionSort {
-	public static <E extends Comparable<? super E>> void sort(E[] e) {
+	public static <E extends Comparable<? super E>> void simpleSort(E[] e) {
 		int j;
 		int length = e.length;
 		for (int i = 1; i < length; i++) {
 			E tmp = e[i]; 
-			for (j = i; j > 0 && e[j].compareTo(e[j - 1]) < 0; j--) {
+			for (j = i; j >= 1 && tmp.compareTo(e[j - 1]) < 0; j--) {
 				e[j] = e[j - 1];
 			}
-			e[j] = tmp;
+			if (j != i) {
+				e[j] = tmp;
+			}
+		}
+	}
+	
+	public static <E extends Comparable<? super E>> void shellSort(E[] e) {
+		int length = e.length;
+		int h, j;
+		for (h = length / 2; h > 0; h /= 2) {
+			for (int i = h; i < length; i++) {
+				E tmp = e[i];
+				for (j = i; j >= h && tmp.compareTo(e[j - h]) < 0; j -= h) {
+					e[j] = e[j - h];
+				}
+				if (j != i) {
+					e[j] = tmp;
+				}
+			}
 		}
 	}
 }
--- src/com/cc/test/TestSort.java
+++ src/com/cc/test/TestSort.java
@@ -1,10 +1,28 @@
 package com.cc.test;
 
+import java.util.Random;
+
+import com.cc.sort.InsertionSort;
+
 public class TestSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] test = {"81", "11", "96", "12",
+				"35", "17", "95", "28", 
+				"58", "41", "75", "15"};
+		Random random = new Random();
+		int tmp = random.nextInt(100);
+		System.out.println(tmp);
+		int[] arr = random.ints(10, 10, 100).toArray();
+		
+		for (String str : test) {
+			System.out.print(str + ", ");
+		}
+		System.out.println();
+		InsertionSort.shellSort(test);
+		for (String str : test) {
+			System.out.print(str + ", ");
+		}
 	}
 
 }
