4b60dd2be3a31bd3a3936fdd169f70da3db3717c
HJKCC <chencheng0816@gmail.com>
2018-04-20 09:16:13 +0800

    annotate the mergeSort

--- src/com/cc/sort/MergeSort.java
+++ src/com/cc/sort/MergeSort.java
@@ -1,6 +1,10 @@
 package com.cc.sort;
 
 public class MergeSort {
+	/**
+	 * 归并排序，主方法
+	 * @param e
+	 */
 	public static <E extends Comparable<? super E>> void sort(E[] e) {
 		int length = e.length;
 		E[] tmpArray = (E[]) new Comparable[length];
@@ -8,22 +12,42 @@ public class MergeSort {
 		mergeSort(e, tmpArray, 0, length - 1);		
 	}
 	
+	/**
+	 * 循环归并，采用分治策略(divide-and-conquer)
+	 * @param srcArray 原数组
+	 * @param resArray 临时数组
+	 * @param left 原数组起始标识
+	 * @param right 原数组结束标识
+	 */
 	private static <E extends Comparable<? super E>> void mergeSort(E[] srcArray, E[] resArray, int left, int right) {
 		if (left < right) {		
 			int center = (left + right) / 2;
+			
+			//分divite
 			mergeSort(srcArray, resArray, left, center);
 			mergeSort(srcArray, resArray, center +1, right);
 			
+			//治conquer
 			merge(srcArray, resArray, left, center, right);
 		}
 	}
 	
+	/**
+	 * 合并两个排序后的子序列
+	 * @param srcArray  原数组
+	 * @param resArray 临时数组
+	 * @param leftPos 原数组起始标识
+	 * @param leftEnd 第一个序列结束标识
+	 * @param rightEnd 原数组结束标识
+	 */
 	private static <E extends Comparable<? super E>> void merge(E[] srcArray, E[] resArray, int leftPos, int leftEnd, int rightEnd) {
-		int startPos = leftPos;
-		int endPos = rightEnd;
+		int startPos = leftPos;  //save the start postion
+		int endPos = rightEnd;  //save the end postion
+		
 		int resPos = leftPos;
 		int rightPos = leftEnd + 1;
 		
+		//main loop
 		while (leftPos <= leftEnd && rightPos <= rightEnd) {
 			if(srcArray[leftPos].compareTo(srcArray[rightPos]) < 0) {
 				resArray[resPos++] = srcArray[leftPos++];
@@ -32,16 +56,17 @@ public class MergeSort {
 			}
 		}
 		
-		while (leftPos <= leftEnd) {
+		while (leftPos <= leftEnd) {   //copy rest of left
 			resArray[resPos++] = srcArray[leftPos++];
 		}
 		
-		while (rightPos <= rightEnd) {
+		while (rightPos <= rightEnd) {   //copy rest of right
 			resArray[resPos++] = srcArray[rightPos++];
 		}
 		
+		//copy from resArray to srcArray
 		for (; startPos <= endPos; startPos++) {
-			srcArray[resPos] = resArray[resPos];
+			srcArray[startPos] = resArray[startPos];
 		}
 	}
 }
--- src/com/cc/test/TestSort.java
+++ src/com/cc/test/TestSort.java
@@ -30,5 +30,4 @@ public class TestSort {
 			System.out.print(str + ", ");
 		}
 	}
-
 }
